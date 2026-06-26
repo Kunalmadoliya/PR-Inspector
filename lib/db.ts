df0358e62811b33617a0b14f61b74dd7a6964c81
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-06-26 22:49:45 +0530

    feat: integrate Prisma ORM with PostgreSQL and initialize database schema and client configuration

---
+++ lib/db.ts
@@ -0,0 +1,20 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+
+function createPrismaClient(){
+   const url = process.env.DATABASE_URL;
+
+   if(!url){
+      throw new Error("DATABASE_URL is not defined");
+   }
+
+   const adapter = new PrismaPg({ connectionString: url });
+   return new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
